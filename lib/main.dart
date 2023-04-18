946c5515c1b5e95c0d7f9d4d798609af17b20a02
Israela608 <israela608@gmail.com>
2023-04-18 16:12:08 +0100

    authentication is done

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@ import 'dart:developer' as devtools show log;
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:instagram_clone/state/auth/backend/authenticator.dart';
+import 'package:instagram_clone/state/auth/provider/auth_state_provider.dart';
+import 'package:instagram_clone/state/auth/provider/is_logged_in_provider.dart';
 
 import 'firebase_options.dart';
 
@@ -15,7 +18,11 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const App());
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
+  );
 }
 
 class App extends StatelessWidget {
@@ -35,38 +42,66 @@ class App extends StatelessWidget {
       ),
       themeMode: ThemeMode.dark,
       debugShowCheckedModeBanner: false,
-      home: const HomePage(),
+      home: Consumer(
+        builder: (context, ref, child) {
+          final isLoggedIn = ref.watch(isLoggedInProvider);
+          isLoggedIn.log();
+          if (isLoggedIn) {
+            return const MainView();
+          } else {
+            return const LoginView();
+          }
+        },
+      ),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+//For when you are already logged in
+class MainView extends StatelessWidget {
+  const MainView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        title: const Text('Main View'),
       ),
-      body: Column(
-        children: [
-          TextButton(
+      body: Consumer(
+        builder: (context, ref, child) {
+          return TextButton(
             onPressed: () async {
-              final result = await Authenticator().loginWithGoogle();
-              result.log();
+              await ref.read(authStateProvider.notifier).logOut();
             },
-            child: const Text('Sign in with Google'),
-          ),
-          TextButton(
-            onPressed: () async {
-              final result = await Authenticator().loginWithFacebook();
-              result.log();
-            },
-            child: const Text('Sign in with Facebook'),
-          ),
-        ],
+            child: const Text(
+              'Logout',
+            ),
+          );
+        },
       ),
+    );
+  }
+}
+
+// For when you are not logged in
+class LoginView extends ConsumerWidget {
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Column(
+      children: [
+        TextButton(
+          onPressed: () =>
+              ref.read(authStateProvider.notifier).loginWithGoogle(),
+          child: const Text('Sign in with Google'),
+        ),
+        TextButton(
+          onPressed: () =>
+              ref.read(authStateProvider.notifier).loginWithFacebook(),
+          child: const Text('Sign in with Facebook'),
+        ),
+      ],
     );
   }
 }
